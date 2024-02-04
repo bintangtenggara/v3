0de318b0db599e1406e511aa729a08cd4b8b1611
bintangtenggara <51829646+bintangtenggara@users.noreply.github.com>
2024-02-05 01:59:24 +0700

    Update hibrido.js

--- js/hibrido.js
+++ js/hibrido.js
@@ -1209,7 +1209,7 @@ if (w.metadatatechnic === "corsproxy") {
                     top: "0px",
                     left: "0px",
  "-webkit-filter": "blur(20px)", 
-                filter: "blur(20px)", //efecto blur desenfoque
+                filter: "blur(10px)", //efecto blur desenfoque
 
                     width: "100%",
                     height: "100%"
